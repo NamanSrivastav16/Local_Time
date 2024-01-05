6f7190e93c4d58fd58aa87105f1398c81826f9b9
Naman <sri.naman07@gmail.com>
2024-01-05 21:45:44 +0530

    Local Time

---
+++ script.js
@@ -0,0 +1,9 @@
+// Two Methods
+const clock = document.getElementById('clock')
+// document.querySelector('#clock')
+
+
+setInterval(function () {
+    let date = new Date();
+    clock.innerHTML = date.toLocaleTimeString();
+}, 1000)
